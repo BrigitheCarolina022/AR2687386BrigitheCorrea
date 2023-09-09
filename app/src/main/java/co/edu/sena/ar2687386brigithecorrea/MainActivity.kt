4280d7499b2bd1d7d9936d9ece756914da1695f1
Taller1_aplicacion_web <brigithecorrea80@gmail.com>
2023-09-09 17:26:34 -0500

    adding google-server.json

--- app/src/main/java/co/edu/sena/ar2687386brigithecorrea/MainActivity.kt
+++ app/src/main/java/co/edu/sena/ar2687386brigithecorrea/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("App_Read_2687386")
+                    Greeting("App_Reader_2687386")
                 }
             }
         }
