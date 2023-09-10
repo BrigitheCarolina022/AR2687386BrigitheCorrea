6cc4e24e5404f3bb6ee3677abcdcbcd62cc645cf
Taller1_aplicacion_web <brigithecorrea80@gmail.com>
2023-09-10 14:04:08 -0500

    updating implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,9 @@ dependencies {
     //add hilt dependencies
     implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-android-compiler:2.48")
+    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
